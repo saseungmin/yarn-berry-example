8268c6f4a094903c7f6bf8fe237c62f2745ac1ea
saseungmin <dbd02169@naver.com>
2023-02-10 23:36:54 +0900

    chore: eslint 룰과 jest 세팅을 변경하라
    - 비효율적이게 사용중이던 eslint 룰 수정
    - swc사용하여 jest 세팅 변경
    - update dependency

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,16 @@
-import { createRoot } from 'react-dom/client';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 import './index.css';
 
-const container = document.getElementById('root');
-const root = createRoot(container!);
-
-root.render(
-  <App />,
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement,
 );
 
-reportWebVitals();
+root.render((
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+));
